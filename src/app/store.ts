d34fbfb82621f4cb0bf29580326afedf63c0390d
yullia-jeon <yullia.jeon@krustuniverse.com>
2022-07-23 14:46:12 +0900

    lint 설정

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,10 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "reducer/counterSlice";
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import counterReducer from 'reducer/counterSlice';
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+    reducer: {
+        counter: counterReducer,
+    },
 });
 
 export type AppDispatch = typeof store.dispatch;
