8d31190585b1e51c01c18eb3a4eb168a0b64f22c
changicho <changicho29@gmail.com>
2022-07-28 23:11:23 +0900

    feat: Create getEvent fetch

---
+++ src/fetchs/getEvent.ts
@@ -0,0 +1,21 @@
+import { Event } from 'types/Event';
+import { Item } from 'types/Items';
+
+const dummyEvent: Event = {
+    id: 1,
+    title: '예시 이벤트',
+    code: '123',
+    type: '?',
+    openAt: '2022-07-22 13:00:00',
+    closeAt: '2022-07-22 13:00:00',
+    status: 'wait',
+    items: [],
+    hitMessage: '당첨',
+    hitImageUrl: '',
+    missMessage: '없음',
+    missImageUrl: '',
+};
+
+export async function getEvent(eventCode: string) {
+    return dummyEvent;
+}
