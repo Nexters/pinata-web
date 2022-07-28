90a596b551d6d1b271f3610e9cea86da53866fda
changicho <changicho29@gmail.com>
2022-07-28 23:12:52 +0900

    feat: Add fetch data in Event page

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import NeedLogin from 'components/event/NeedLogin';
+import Waiting from 'components/event/Waiting';
 
-const Event: React.FC = () => {
-    const isLoggined = false;
+import { getEvent } from 'fetchs/getEvent';
+
+import { Event } from 'types/Event';
+
+const EventPage: React.FC = () => {
+    const isLoggined = true;
+    const [event, setEvent] = useState<Event>();
+
+    // 여기서 이벤트 정보 호출후 상태 만듬
+    useEffect(() => {
+        getEvent('123').then((event:Event)=>{
+            setEvent(event)
+        })
+    },[])
 
     if (!isLoggined) {
         return <NeedLogin />;
+    }
+
+    if (event && event.status === 'wait') {
+        return <Waiting event={event} />;
     }
 
     return (
@@ -16,4 +33,4 @@ const Event: React.FC = () => {
     );
 };
 
-export default Event;
+export default EventPage;
