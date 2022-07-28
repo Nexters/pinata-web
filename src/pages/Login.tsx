a18ec41f895828f83b7c1cdbb7a671bd2c2d34a9
yujeong-jeon <workingnewjeong@gmail.com>
2022-07-28 23:59:26 +0900

    로그인 되어있으면 메인 페이지로 리디렉트

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,13 @@
+import ROUTE from '$constants/route'
+import useKakaoLogin from '$hooks/useKakaoLogin'
+import {Navigate} from 'react-router-dom'
 import Main from './Main'
 
 const Login = () => {
+    const {isLogined} = useKakaoLogin()
+    if (isLogined) {
+        return <Navigate to={ROUTE.MAIN} />
+    }
     return <Main />
 }
 
