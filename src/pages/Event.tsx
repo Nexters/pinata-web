bfac5faf8e8597a9bfa823dfe497c53027c68cfe
changicho <changicho29@gmail.com>
2022-07-30 14:18:19 +0900

    feat: Render Closed

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 
 import NeedLogin from '$components/event/NeedLogin'
 import Waiting from '$components/event/Waiting'
+import Closed from '$components/event/Closed'
 
 import {getEvent} from '$fetchs/getEvent'
 
@@ -9,17 +10,24 @@ import {Event} from '$types/Event'
 
 const EventPage: React.FC = () => {
     const isLoggined = true
+    const isClosed = true;
     const [event, setEvent] = useState<Event>()
 
     // 여기서 이벤트 정보 호출후 상태 만듬
     useEffect(() => {
         getEvent('123').then((event: Event) => {
+            console.log(event)
             setEvent(event)
         })
     }, [])
 
+
     if (!isLoggined) {
         return <NeedLogin />
+    }
+
+    if(isClosed){
+        return <Closed />
     }
 
     if (event && event.status === 'wait') {
