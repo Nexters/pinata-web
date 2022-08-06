83c03bb192e8affeaf39494e75173feaa4537f26
changicho <changicho29@gmail.com>
2022-08-06 13:38:03 +0900

    feat: Set min height for scroll pages

--- src/layout/MobileWrapper.tsx
+++ src/layout/MobileWrapper.tsx
@@ -1,35 +1,35 @@
-import React, { ReactNode } from 'react';
+import React, {ReactNode} from 'react'
 
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background: #aaa;
+    width: 100vw;
+    min-height: 100vh;
+    background: #aaa;
 
-  display: flex;
-  justify-content: center;
+    display: flex;
+    justify-content: center;
 
-  padding: 0;
-  margin: 0;
-`;
+    padding: 0;
+    margin: 0;
+`
 
 const MobileScreen = styled.div`
-  position:relative;
-  width: 480px;
-  height: 100%;
+    position: relative;
+    width: 480px;
+    min-height: 100vh;
 
-  background: #fff;
-`;
+    background: #fff;
+`
 
 type Props = {
-  children?: ReactNode;
-};
+    children?: ReactNode
+}
 
-export const MobileWrapper: React.FC<Props> = ({ children }) => {
+export const MobileWrapper: React.FC<Props> = ({children}) => {
     return (
         <Wrapper>
             <MobileScreen>{children}</MobileScreen>
         </Wrapper>
-    );
-};
+    )
+}
