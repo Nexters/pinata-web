6724c46079e3434f506460560c8b2e5d020b72c5
yujeong-jeon <workingnewjeong@gmail.com>
2022-08-09 14:18:42 +0900

    Input 컴포넌트 추가

--- src/pages/CreateEvent.tsx
+++ src/pages/CreateEvent.tsx
@@ -1,19 +1,33 @@
-import InfoCircleIcon from '$assets/icons/InfoCircleIcon';
-import Flex from '$components/commons/Flex';
-import { Section, SectionTitle } from '$components/commons/Section';
-import LayoutWrapper from '$layout/LayoutWrapper';
-import React from 'react';
-import styled from 'styled-components';
+import InfoCircleIcon from '$assets/icons/InfoCircleIcon'
+import Flex from '$components/commons/Flex'
+import {Section, SectionTitle} from '$components/commons/Section'
+import Input from '$components/eventForm/Input'
+import LayoutWrapper from '$layout/LayoutWrapper'
+import React from 'react'
+import {useState} from 'react'
+import styled from 'styled-components'
 
 const CreateEvent: React.FC = () => {
+    const [title, setTitle] = useState('')
+
     return (
         <LayoutWrapper isWhite={false} withBorderBottom>
             <Container>
                 <Section>
-                    <SectionTitle>이벤트 제목을 입력하세요</SectionTitle>
+                    <SectionTitle marginBottom={16}>이벤트 제목을 입력하세요</SectionTitle>
+                    <Input value={title} onChange={(e) => setTitle(e.target.value)} />
                 </Section>
                 <Section marginTop={40}>
-                    <SectionTitle>이벤트 진행 날짜 및 시간을 정해보세요</SectionTitle>
+                    <SectionTitle marginBottom={16}>이벤트 진행 날짜 및 시간을 정해보세요</SectionTitle>
+                    <Input
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        label={'시작'}
+                        style={{
+                            marginBottom: 12,
+                        }}
+                    />
+                    <Input value={title} onChange={(e) => setTitle(e.target.value)} label={'종료'} />
                 </Section>
                 <Section marginTop={40}>
                     <SectionTitle>이벤트 모드를 선택하세요</SectionTitle>
@@ -23,15 +37,17 @@ const CreateEvent: React.FC = () => {
                 </Section>
                 <Section marginTop={40}>
                     <SectionTitle>
-                        <Flex direction='row' justifyContent={'space-between'}>
-                        당첨 안내 이미지와 메시지를 등록하세요<InfoCircleIcon size={16} color={'#1b1b1e80'} />
+                        <Flex direction="row" justifyContent={'space-between'}>
+                            당첨 안내 이미지와 메시지를 등록하세요
+                            <InfoCircleIcon size={16} color={'#1b1b1e80'} />
                         </Flex>
                     </SectionTitle>
                 </Section>
                 <Section marginTop={40}>
                     <SectionTitle>
-                        <Flex direction='row' justifyContent={'space-between'}>
-                        탈락 안내 이미지와 메시지를 등록하세요<InfoCircleIcon size={16} color={'#1b1b1e80'} />
+                        <Flex direction="row" justifyContent={'space-between'}>
+                            탈락 안내 이미지와 메시지를 등록하세요
+                            <InfoCircleIcon size={16} color={'#1b1b1e80'} />
                         </Flex>
                     </SectionTitle>
                 </Section>
@@ -41,7 +57,7 @@ const CreateEvent: React.FC = () => {
 }
 
 const Container = styled.div`
-    padding: 30px 20px
+    padding: 30px 20px;
 `
 
-export default CreateEvent;
+export default CreateEvent
--- src/styles/typos.ts
+++ src/styles/typos.ts
@@ -1,4 +1,4 @@
-import { css, FlattenSimpleInterpolation } from 'styled-components';
+import {css, FlattenSimpleInterpolation} from 'styled-components'
 
 const pretendardBase = css`
     font-family: 'Pretendard';
@@ -18,14 +18,15 @@ const createTypo = (base: Typo, size: number, lineHeight: number, weight: number
 
 export const typos = {
     pretendard: {
-        '12.18.400' : createTypo(pretendardBase, 12, 18, 400),
+        '12.18.400': createTypo(pretendardBase, 12, 18, 400),
         '12.19.600': createTypo(pretendardBase, 12, 19, 600),
         '13.19.400': createTypo(pretendardBase, 13, 19, 400),
+        '13.26.500': createTypo(pretendardBase, 13, 26, 500),
         '14.19.400': createTypo(pretendardBase, 14, 19, 400),
         '16.19.700': createTypo(pretendardBase, 16, 19, 700),
         '16.26.700': createTypo(pretendardBase, 16, 26, 700),
         '18.19.700': createTypo(pretendardBase, 18, 19, 700),
         '22.18.700': createTypo(pretendardBase, 22, 18, 700),
-        '22.32.700': createTypo(pretendardBase, 22, 32, 700)
-    }
+        '22.32.700': createTypo(pretendardBase, 22, 32, 700),
+    },
 } as const
