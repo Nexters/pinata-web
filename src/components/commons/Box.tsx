c8af542ea132ed645a83dbfe28db9cc2569f27f0
yujeongJeon <54320809+yujeongJeon@users.noreply.github.com>
2022-08-10 15:12:05 +0900

    Dialog 컴포넌트 추가 (#13)

--- src/components/commons/Box.tsx
+++ src/components/commons/Box.tsx
@@ -1,3 +1,4 @@
+import {Typo} from '$styles/typos'
 import styled, {css, CSSProperties} from 'styled-components'
 
 type BoxProps = {
@@ -6,13 +7,15 @@ type BoxProps = {
     height?: number | string
     padding?: CSSProperties['padding']
     style?: CSSProperties
+    typo?: Typo
 }
 
 export const Box = styled.div<BoxProps>`
-    ${({backgroundColor, width, height, padding}) => css`
+    ${({backgroundColor, width, height, padding, typo}) => css`
         background-color: ${backgroundColor};
         width: ${width !== undefined ? (typeof width === 'number' ? width + 'px' : width) : undefined};
         height: ${height !== undefined ? (typeof height === 'number' ? height + 'px' : height) : undefined};
         padding: ${padding};
+        ${typo};
     `}
 `
