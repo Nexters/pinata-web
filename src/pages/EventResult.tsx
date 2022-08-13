73786129113b06569b14e6d3dddc827580532726
yullia-jeon <yullia.jeon@krosslab.io>
2022-08-13 13:59:14 +0900

    imageUrl 수정

--- src/pages/EventResult.tsx
+++ src/pages/EventResult.tsx
@@ -64,7 +64,7 @@ const EventResult = () => {
                 <ParticipatedNotice />
             ) : (
                 <Card>
-                    <Card.Image src={'/images/example-result-card.png'} description={'Image 설명'} withClose />
+                    <Card.Image src={'images/example-result-card.png'} description={'Image 설명'} withClose />
                     <Card.Content>
                         <Badge text={'탈락'} type="default" marginBottom={6} />
                         <Card.Title>카드 제목</Card.Title>
