86691d7b2881fa17c4b60230ac250fd26e8885d3
yullia-jeon <yullia.jeon@krosslab.io>
2022-08-13 19:38:04 +0900

    merge client.ts

--- src/util/client.ts
+++ src/util/client.ts
@@ -33,6 +33,7 @@ const rejectInterceptor = (error: AxiosError) => {
 }
 
 const client = axios.create({
+    withCredentials: true,
     baseURL: process.env.REACT_APP_ENV === 'production' ?  process.env.REACT_APP_API_URL : ''
 })
 
