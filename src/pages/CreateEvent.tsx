62b9cad03547fa538c2e2ebf764958eebda944ae
yujeongJeon <54320809+yujeongJeon@users.noreply.github.com>
2022-08-13 13:09:02 +0900

    develop -> main (#16)
    * 메뉴 링크에 피나타 아이콘 추가
    
    * Menu 컴포넌트 오픈 시 scroll 방지
    
    * CreateEvent 페이지 기본 구조 잡기
    
    * 헤더 로고 클릭 시 메인 화면으로 이동
    
    * 이벤트생성 페이지 마크업 (#12)
    
    * Input 컴포넌트 추가
    
    * RadioForm 추가
    
    * 상품 리스트 컴포넌트 추가
    
    * SelectBox 추가
    - headless ui 사용
    - 이벤트 생성 페이지 마크업 완성
    
    * typo 누락된 컴포넌트 적용
    
    * Dialog 컴포넌트 추가 (#13)
    
    * 개설한 이벤트 페이지 작업
    
    * 참여한 이벤트 페이지 작업
    
    * 새 디자인 적용
    
    * deploy 준비
    - favicon 추가
    - blocked(mixed-content) 오류 수정
    
    * react hook form 으로 입력값 상태 관리 (#15)
    
    * react hook form으로 입력값 상태 관리
    
    * custom radio에 hook form 적용
    
    Co-authored-by: yullia-jeon <yullia.jeon@krosslab.io>
    
    * nvmrc 추가
    
    Co-authored-by: yullia-jeon <yullia.jeon@krosslab.io>

--- src/pages/CreateEvent.tsx
+++ src/pages/CreateEvent.tsx
@@ -10,8 +10,8 @@ import LayoutWrapper from '$layout/LayoutWrapper'
 import {typos} from '$styles/typos'
 import {extractProp} from '$util/common'
 import React from 'react'
-import {useState} from 'react'
 import styled, {css} from 'styled-components'
+import {useForm} from 'react-hook-form'
 
 const radioCommonStyle = css`
     border-radius: 15px;
@@ -37,103 +37,129 @@ const DEMO_GIFTS = [
     },
 ]
 
+enum EventMode {
+    RANDOM = 1,
+    FIFO
+}
+
+export interface EventForm {
+    title: string
+    startDate: Date
+    endDate: Date
+    hitDesc: string
+    failDesc: string
+    eventMode: EventMode
+}
+
+const required = true
+
 const CreateEvent: React.FC = () => {
-    const [title, setTitle] = useState('')
-    const [hitDesc, setHitDesc] = useState('')
-    const [failDesc, setFailDesc] = useState('')
+    const {register, handleSubmit, setValue} = useForm<EventForm>()
+
+    const onSubmit = (data: EventForm) => {
+        console.log(data)
+    }
+
+    const onSelect = (value: number) => {
+        setValue('eventMode', value)
+    }
 
     return (
         <LayoutWrapper isWhite={false} withBorderBottom>
             <Container>
-                <Section>
-                    <SectionTitle marginBottom={16}>이벤트 제목을 입력하세요</SectionTitle>
-                    <Input value={title} onChange={(e) => setTitle(e.target.value)} />
-                </Section>
-                <Section marginTop={40}>
-                    <SectionTitle marginBottom={16}>이벤트 진행 날짜 및 시간을 정해보세요</SectionTitle>
-                    <Input
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        label={'시작'}
-                        style={{
-                            marginBottom: 12,
-                        }}
-                    />
-                    <Input value={title} onChange={(e) => setTitle(e.target.value)} label={'종료'} />
-                </Section>
-                <Section marginTop={40}>
-                    <SectionTitle marginBottom={16}>이벤트 모드를 선택하세요</SectionTitle>
-                    <RadioForm values={[1, 2]}>
-                        <RadioForm.Item
-                            width={162}
-                            height={100}
-                            value={1}
-                            selectedStyle={radioSelectStyle}
-                            unselectedStyle={radioDefaultStyle}
-                            style={radioCommonStyle}>
-                            랜덤 모드
-                        </RadioForm.Item>
-                        <RadioForm.Item
-                            width={162}
-                            height={100}
-                            value={2}
-                            selectedStyle={radioSelectStyle}
-                            unselectedStyle={radioDefaultStyle}
-                            style={radioCommonStyle}>
-                            선착순 모드
-                        </RadioForm.Item>
-                    </RadioForm>
-                </Section>
-                <Section marginTop={40}>
-                    <SectionTitle marginBottom={16}>당첨 상품을 등록하세요</SectionTitle>
-                    <Flex direction="column">
-                        <GiftDialog />
-                        <GiftList items={DEMO_GIFTS} />
-                        <Totals>
-                            총 상품 수령 인원
-                            <NumberHighlight>{DEMO_GIFTS.length}명</NumberHighlight>
-                        </Totals>
-                    </Flex>
-                </Section>
-                <Section marginTop={40}>
-                    <SectionTitle marginBottom={16}>
-                        <Flex direction="row" justifyContent={'space-between'}>
-                            당첨 안내 이미지와 메시지를 등록하세요
-                            <InfoCircleIcon size={16} color={'#1b1b1e80'} />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Section>
+                        <SectionTitle marginBottom={16}>이벤트 제목을 입력하세요</SectionTitle>
+                        <Input {...register('title', {required})} type="text" />
+                    </Section>
+                    <Section marginTop={40}>
+                        <SectionTitle marginBottom={16}>이벤트 진행 날짜 및 시간을 정해보세요</SectionTitle>
+                        <Input
+                            {...register('startDate', {required})}
+                            label={'시작'}
+                            style={{
+                                marginBottom: 12,
+                            }}
+                            type="date"
+                        />
+                        <Input {...register('endDate', {required})} label={'종료'} type="date" />
+                    </Section>
+                    <Section marginTop={40}>
+                        <SectionTitle marginBottom={16}>이벤트 모드를 선택하세요</SectionTitle>
+                        <RadioForm values={[EventMode.RANDOM, EventMode.FIFO]} defaultValue={EventMode.RANDOM}>
+                            <RadioForm.Item
+                                onSelect={onSelect}
+                                width={162}
+                                height={100}
+                                value={1}
+                                selectedStyle={radioSelectStyle}
+                                unselectedStyle={radioDefaultStyle}
+                                style={radioCommonStyle}>
+                                랜덤 모드
+                            </RadioForm.Item>
+                            <RadioForm.Item
+                                onSelect={onSelect}
+                                width={162}
+                                height={100}
+                                value={2}
+                                selectedStyle={radioSelectStyle}
+                                unselectedStyle={radioDefaultStyle}
+                                style={radioCommonStyle}>
+                                선착순 모드
+                            </RadioForm.Item>
+                        </RadioForm>
+                    </Section>
+                    <Section marginTop={40}>
+                        <SectionTitle marginBottom={16}>당첨 상품을 등록하세요</SectionTitle>
+                        <Flex direction="column">
+                            <GiftDialog />
+                            <GiftList items={DEMO_GIFTS} />
+                            <Totals>
+                                총 상품 수령 인원
+                                <NumberHighlight>{DEMO_GIFTS.length}명</NumberHighlight>
+                            </Totals>
                         </Flex>
-                    </SectionTitle>
-                    <CardListForm
-                        images={[]}
-                        inputProps={{
-                            value: hitDesc,
-                            onChange: (e) => setHitDesc(e.target.value),
-                            placeholder: '이벤트 당첨 안내 및 축하 메시지를 적어주세요',
-                        }}
-                        label={'당첨'}
-                    />
-                </Section>
-                <Section marginTop={40}>
-                    <SectionTitle marginBottom={16}>
-                        <Flex direction="row" justifyContent={'space-between'}>
-                            탈락 안내 이미지와 메시지를 등록하세요
-                            <InfoCircleIcon size={16} color={'#1b1b1e80'} />
-                        </Flex>
-                    </SectionTitle>
-                    <CardListForm
-                        images={[]}
-                        inputProps={{
-                            value: failDesc,
-                            onChange: (e) => setFailDesc(e.target.value),
-                            placeholder: '이벤트 탈락 안내 및 위로 메시지를 적어주세요',
-                        }}
-                        label={'탈락'}
-                    />
-                </Section>
-                <Section marginTop={30}>
-                    <Button color={'dark'} height={52}>
-                        이벤트 개설하기
-                    </Button>
-                </Section>
+                    </Section>
+                    <Section marginTop={40}>
+                        <SectionTitle marginBottom={16}>
+                            <Flex direction="row" justifyContent={'space-between'}>
+                                당첨 안내 이미지와 메시지를 등록하세요
+                                <InfoCircleIcon size={16} color={'#1b1b1e80'} />
+                            </Flex>
+                        </SectionTitle>
+                        <CardListForm
+                            images={[]}
+                            inputProps={{
+                                ...register('hitDesc', {required}),
+                                type: 'text',
+                                placeholder: '이벤트 당첨 안내 및 축하 메시지를 적어주세요',
+                            }}
+                            label={'당첨'}
+                        />
+                    </Section>
+                    <Section marginTop={40}>
+                        <SectionTitle marginBottom={16}>
+                            <Flex direction="row" justifyContent={'space-between'}>
+                                탈락 안내 이미지와 메시지를 등록하세요
+                                <InfoCircleIcon size={16} color={'#1b1b1e80'} />
+                            </Flex>
+                        </SectionTitle>
+                        <CardListForm
+                            images={[]}
+                            inputProps={{
+                                ...register('failDesc', {required}),
+                                type: 'text',
+                                placeholder: '이벤트 탈락 안내 및 위로 메시지를 적어주세요',
+                            }}
+                            label={'탈락'}
+                        />
+                    </Section>
+                    <Section marginTop={30}>
+                        <Button type="submit" color={'dark'} height={52}>
+                            이벤트 개설하기
+                        </Button>
+                    </Section>
+                </form>
             </Container>
         </LayoutWrapper>
     )
