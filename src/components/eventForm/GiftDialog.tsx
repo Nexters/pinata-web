2928e163b7f86f8ad5b6a78c5f8344735d9d4fe6
yujeongJeon <54320809+yujeongJeon@users.noreply.github.com>
2022-08-16 00:52:13 +0900

    이벤트 개설 > 상품 등록 Dialog 로직 추가 (#34)
    * Dialog 를 React.Portal로 띄우기
    
    * 상품 등록 로직 구현
    
    * 상품 등록 후 등록Dialog의 입력값 초기화
    - mode props 추가
    - Dialog에 onOpen props 추가
    
    Co-authored-by: yullia-jeon <yullia.jeon@krosslab.io>

--- src/components/eventForm/GiftDialog.tsx
+++ src/components/eventForm/GiftDialog.tsx
@@ -1,61 +1,128 @@
+import { GiftItem } from '$api/gift'
 import PlusIcon from '$assets/icons/PlusIcon'
 import {Box} from '$components/commons/Box'
+import Flex from '$components/commons/Flex'
 import Dialog from '$components/dialog/Dialog'
 import { colors } from '$styles/colors'
 import {typos} from '$styles/typos'
 import {extractProp} from '$util/common'
+import { MouseEventHandler, ReactNode, useRef } from 'react'
 import { useForm } from 'react-hook-form'
-import styled, {css} from 'styled-components'
+import styled from 'styled-components'
+import ImageUploader from './ImageUploader'
 import Input from './Input'
 
-interface GiftForm {
-    name: string
+type GiftItemForm = Pick<GiftItem, 'title' | 'imageUrl'>
+
+type GiftDialogProps = {
+    addItem(item: GiftItemForm): void
+    defaultValues?: GiftItemForm
+    children: ReactNode
+    mode: 'add' | 'modify'
 }
 
-const GiftDialog = () => {
-    const {register, handleSubmit} = useForm<GiftForm>()
-    const onSubmit = (data: GiftForm) => {
-        console.log(data)
+const GiftDialogButton = ({onClick, closeDialog}: {
+    onClick(): void
+    closeDialog?(): void
+}) => {
+    const handleClick = () => {
+        onClick()
+        typeof closeDialog === 'function' && closeDialog()
     }
+    return (
+        <Button onClick={handleClick} height={52}>
+            등록하기
+        </Button>
+    )
+}
+
+const GiftDialog = ({addItem, defaultValues, children, mode}: GiftDialogProps) => {
+    const {register, getValues, setValue, watch, reset} = useForm<GiftItemForm>({
+        defaultValues
+    })
+
+    const imageUrl = watch('imageUrl')
+
+    const imageUploaderRef = useRef<HTMLInputElement>(null)
+
+    const uploadImage: MouseEventHandler<HTMLDivElement> = (e) => {
+        e.preventDefault()
+        imageUploaderRef.current && imageUploaderRef.current.click()
+    }
+
+    const handleClick = () => {
+        const [title, imageUrl] = getValues(['title', 'imageUrl'])
+        if (!imageUrl) {
+            return
+        }
+        addItem({title, imageUrl})
+    }
+
+    const handleUpload = (urls: string[]) => {
+        setValue('imageUrl', urls[0])
+    }
+
+    const setValueByMode = () => {
+        if (mode === 'add') {
+            reset({
+                title: '',
+                imageUrl: undefined
+            })
+        }
+    }
+
     return (
         <Dialog>
             <Dialog.Button width={'100%'}>
-                <Button color={'default'} height={52}>
-                    <PlusIcon size={19} color={colors.white} />
-                </Button>
+                {children}
             </Dialog.Button>
-            <Dialog.Content width={335}>
+            <Dialog.Content width={335} onOpen={setValueByMode}>
                 <Dialog.Title>당첨 상품 이미지 및 이름 등록</Dialog.Title>
                 <DialogSubTitle>선물하실 상품 이미지를 등록하세요</DialogSubTitle>
-                <Button color={'dark'} height={90}>
-                    <PlusIcon size={19} color={colors.white} />
-                    <Box
-                        typo={typos.pretendard['12.20.500']}
-                        style={{
-                            marginTop: 5,
-                        }}>
-                        상품 이미지 등록하기
-                    </Box>
-                </Button>
+                {
+                    imageUrl
+                    ? <ImageViewerContainer>
+                        <img src={imageUrl} width={90} height={90} alt='gift' />
+                    </ImageViewerContainer>
+                    : <Button height={90} onClick={uploadImage}>
+                            <PlusIcon size={19} color={colors.white} />
+                            <Box
+                                typo={typos.pretendard['12.20.500']}
+                                style={{
+                                    marginTop: 5,
+                                }}>
+                                상품 이미지 등록하기
+                            </Box>
+                        </Button>
+                }
+                <ImageUploader onUpload={handleUpload} ref={imageUploaderRef} />
                 <Divider />
                 <DialogSubTitle>선물하실 상품 이름을 적어주세요</DialogSubTitle>
-                <form onSubmit={handleSubmit(onSubmit)}>
                 <Input
-                    {...register('name', {required: true})}
+                    {...register('title', {required: true})}
                     type='text'
                     placeholder="최대 20글자"
                     style={{
                         marginBottom: 36,
                     }}
                 />
-                <Button type='submit' color="dark" height={52}>
-                    등록하기
-                </Button>
-                </form>
+                <GiftDialogButton onClick={handleClick} />
             </Dialog.Content>
         </Dialog>
     )
 }
+
+const ImageViewerContainer = styled(Flex).attrs({
+    direction: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+})`
+    & img {
+        border: 1px solid rgba(255, 255, 255, .3);
+        padding: 2px;
+    }
+    gap: 2px;
+`
 
 const Divider = styled.hr`
     border: 0.5px solid rgba(255, 255, 255, 0.1);
@@ -67,8 +134,7 @@ const DialogSubTitle = styled.div`
     color: ${colors.white};
     margin-bottom: 10px;
 `
-const Button = styled.button<{
-    color: 'default' | 'dark'
+const Button = styled.div<{
     height: number
 }>`
     display: flex;
@@ -78,16 +144,8 @@ const Button = styled.button<{
     cursor: pointer;
     outline: none;
     border: none;
-    ${({color}) =>
-        color === 'default'
-            ? css`
-                  background: ${colors.black[300]};
-                  color: ${colors.white};
-              `
-            : css`
-                  background: ${colors.black[700]};
-                  color: ${colors.white};
-              `}
+    background: ${colors.black[700]};
+    color: ${colors.white};
     border-radius: 15px;
     height: ${extractProp('height')}px;
     width: 100%;
