c831506a877eed180dd254311ffae0538b954671
changicho <changicho29@gmail.com>
2022-08-16 20:05:08 +0900

    feat: Add useParams hook

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -7,7 +7,7 @@ import Participation from '$components/event/Participation'
 import useKakaoLogin from '$hooks/useKakaoLogin'
 import {EventResponse, participateEvent} from '$api/event'
 import ROUTE from '$constants/route'
-import {Navigate} from 'react-router-dom'
+import {Navigate, useParams} from 'react-router-dom'
 import useAsyncError from '$hooks/useAsyncError'
 import useAuthToken from '$hooks/useAuthToken'
 import InvalidCode from '$components/event/InvalidCode'
@@ -17,6 +17,7 @@ const EventPage: React.FC = () => {
     const [event, setEvent] = useState<EventResponse>()
     const [isError, setIsError] = useState<boolean>(false)
     const {isLogined} = useKakaoLogin()
+    const params = useParams()
 
     const isClosed = event && event.status === 'CLOSED'
     const isWaiting = event && event.status === 'WAIT'
@@ -27,12 +28,14 @@ const EventPage: React.FC = () => {
     const token = useAuthToken()
 
     useEffect(() => {
-        const {pathname} = window.location
-
-        const paths = pathname.split('/')
-        const eventCode = paths[2]
+        const eventCode = params.eventcode
 
         if (!token) return
+        if (!eventCode) {
+            setIsError(true)
+            return
+        }
+
         participateEvent(eventCode, token).then((res) => {
             if (res.result === 'FAIL') {
                 setIsError(true)
@@ -42,7 +45,7 @@ const EventPage: React.FC = () => {
             const event = res.data
             setEvent(event)
         })
-    }, [token])
+    }, [params.eventcode, token])
 
     if (isError) {
         return <InvalidCode />
