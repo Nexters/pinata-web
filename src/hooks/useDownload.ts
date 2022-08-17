3ac76a79e583268e73b33be8607540649f278b8d
yullia-jeon <yullia.jeon@krosslab.io>
2022-08-17 12:46:47 +0900

    이미지 다운로드 기능 구현

--- src/hooks/useDownload.ts
+++ src/hooks/useDownload.ts
@@ -1,60 +1,35 @@
+import client from '$util/client';
 import { useCallback } from 'react';
+import useAuthToken from './useAuthToken';
 
 const useDownload = () => {
-    function getDataUrl(img: any) {
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx && ctx.drawImage(img, 0, 0);
-        return canvas.toDataURL('image/jpeg');
-     }
+    const accessToken = useAuthToken()
 
-    const downloadFromUrl = useCallback((source: string) => {
-        const img = document.createElement('img')
+    const downloadFromUrl = useCallback(async (source: string) => {
+        const fileName = source.split('/').pop()
 
-        img.src = source
-        img.crossOrigin = 'Anonymous'
-        img.style.visibility = 'hidden'
-        img.onload = (e) => {
-            const target = e.currentTarget
-            const dataUrl = target ? getDataUrl(target) : null
-            
-            console.log(dataUrl)
-        }
+        const response = await client.get<Blob, Blob>(
+            `/api/v1/images/download/${fileName}`,
+            { 
+                responseType: 'blob', 
+                timeout: 30000, 
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                } 
+            },
+        )
 
-        document.body.appendChild(img)
+        const name = `pinata_${fileName}`
+        const url = URL.createObjectURL(response)
 
-        // fetch로 받은 바이너리 파일로 다운로드하는 코드
-        /**
-         * 
-            const response = await request(
-                `/request/img`,
-                { data },
-                { responseType: 'blob, timeout: 30000 },
-            )
-
-            const name = `proposed_blocks_${address}_${date}.csv`
-            const url = URL.createObjectURL(response.data)
-
-            const link = document.createElement('a')
-            link.href = url
-            link.setAttribute('download', name)
-            link.style.cssText = 'display:none'
-            document.body.appendChild(link)
-            link.click()
-            link.remove()
-         */
-
-        // const fileName = source.split('/').pop()
-        // if (fileName) {
-        //     const anchor = document.createElement('a')
-        //     anchor.href = source
-        //     anchor.setAttribute('download', fileName)
-        //     document.body.appendChild(anchor)
-        //     anchor.click()
-        //     anchor.remove()
-        // }
+        const link = document.createElement('a')
+        link.href = url
+        link.setAttribute('download', name)
+        link.style.cssText = 'display:none'
+        document.body.appendChild(link)
+        link.click()
+        link.remove()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return downloadFromUrl
