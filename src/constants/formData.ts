acd1a15f01d8fb3f6f8b60f981d5f7d5e0fc1c52
yujeong-jeon <workingnewjeong@gmail.com>
2022-08-20 03:31:44 +0900

    당첨, 탈락 이미지 리스트 업데이트

--- src/constants/formData.ts
+++ src/constants/formData.ts
@@ -1,2 +1,12 @@
-export const DEFAULT_HIT_IMAGES = ['https://bucket-pinata.s3.ap-northeast-2.amazonaws.com/default_hit_image.svg']
-export const DEFAULT_MISS_IMAGES = ['https://bucket-pinata.s3.ap-northeast-2.amazonaws.com/default_miss_image.svg']
+export const DEFAULT_HIT_IMAGES = [
+    'https://bucket-pinata.s3.ap-northeast-2.amazonaws.com/hit_image_1.svg',
+    'https://bucket-pinata.s3.ap-northeast-2.amazonaws.com/hit_image_2.svg',
+    'https://bucket-pinata.s3.ap-northeast-2.amazonaws.com/hit_image_3.svg',
+    'https://bucket-pinata.s3.ap-northeast-2.amazonaws.com/hit_image_4.svg',
+]
+export const DEFAULT_MISS_IMAGES = [
+    'https://bucket-pinata.s3.ap-northeast-2.amazonaws.com/miss_image_1.svg',
+    'https://bucket-pinata.s3.ap-northeast-2.amazonaws.com/miss_image_2.svg',
+    'https://bucket-pinata.s3.ap-northeast-2.amazonaws.com/miss_image_3.svg',
+    'https://bucket-pinata.s3.ap-northeast-2.amazonaws.com/miss_image_4.svg',
+]
